ed273ada31766b431cdf17e9878b27f6191cbfce
Dhanushvel123 <vdhanush480@gmail.com>
2025-05-20 14:23:05 +0530

    Add gh-pages for deployment and update homepage in package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, Container, Button } from 'react-bootstrap';
 import { FaUserCircle } from 'react-icons/fa';
 import { jwtDecode } from 'jwt-decode'; // <-- Named import
